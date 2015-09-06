90a4aa22eb41db355f0c46989a73c6bc2ee8cd44
boudewijne <boudewijn@boudewijnector.nl>
2015-09-06 23:22:51 +0200

    more comments.

--- tweakers-challenge/9.py
+++ tweakers-challenge/9.py
@@ -2,4 +2,10 @@
 #Hoeveel enen komen er voor in de binaire representatie van dit getal?
 #Antwoord als positief integer, bijvoorbeeld: 15000
 
+#1: reken het getal uit.
+#2: maak dat binair
+#3: maak er een string van
+#4: filter alles wat niet "1" is uit (dat doet de filter met lambda functie)
+#5: tel wat over blijft
+
 print (len(filter(lambda x: x== "1", str(bin(5**128)))))
